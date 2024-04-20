7e9c58d2c2a79615531c84ea2750638e7563a79c
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-04-20 20:48:49 +0530

    added link to login button

--- src/app/components/NavbarCustom.jsx
+++ src/app/components/NavbarCustom.jsx
@@ -32,7 +32,7 @@ export default function NavbarCustom(){
       </NavbarContent>
       <NavbarContent justify="end">
         <NavbarItem className="hidden lg:flex">
-          <Link href="#">Login</Link>
+          <Link href="/login">Login</Link>
         </NavbarItem>
         <NavbarItem>
           <Button as={Link} color="primary" href="/login" variant="flat">
