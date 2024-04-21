e07830d188d6b7759b8c374ad5a89b78d381535d
gvarshithreddy <gvarshithreddy8@gmail.com>
2024-04-21 11:48:17 +0530

    added emoji in page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,20 +1,21 @@
 'use client'
 import {Button} from "@nextui-org/react";
 import NavbarCustom from "./components/NavbarCustom";
+import Link from "next/link";
 export default function Home() {
   return (
     <>
     <NavbarCustom/>
     <div className="mt-60  mx-auto  rounded-md">
     <div className="align-middle justify-center text-center w-50 mx-auto my-10">
-      <h1 className="my-10 text-5xl">Hello Learner!</h1>
+      <h1 className="my-10 text-5xl">Hello Learner! 👋🏿</h1>
       <p className="text-blue-600">Get Started with the most amazing tool with which you can make your learnier wayyyyy faster and wayyyy easier.</p>
       <button />
     </div>
 
     <div className="text-center">
-      <Button color="primary" className="hover:bg-blue-400 mx-5">Get Started</Button>
-      <Button>Go to notes</Button>
+      <Button as={Link} color="primary" href="/login" className="hover:bg-blue-400 mx-5">Get Started</Button>
+      <Button as={Link} href="/notes">Go to notes</Button>
     </div>
     </div>
     
